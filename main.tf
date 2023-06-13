a820abb69d1c29b577206b5a3148b28c844e6951
Perizat Amantaeva <126831825+perizatamantaeva@users.noreply.github.com>
2023-06-13 18:17:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-2"
 
 
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
+ami = "ami-0e820afa569e84cc1"
 instance_type = "t2.micro"
 availability_zone = "us-east-2a"
 key_name = "jenkins"
